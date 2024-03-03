448a0cbd578e086d648d36ddc97a4e334babf398
Adam <adam.mehdaouijorge@gmail.com>
2024-03-03 20:03:02 +0100

    dist files

--- api-gateway/dist/app.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { ClientProxy } from '@nestjs/microservices';
-export declare class AppController {
-    private readonly helloClient;
-    constructor(helloClient: ClientProxy);
-    getHello(): import("rxjs").Observable<string>;
-}
