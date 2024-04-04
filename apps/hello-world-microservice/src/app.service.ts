d50031cfce04eb72ae9a2ec24e02e93480695198
Adam <adam.mehdaouijorge@gmail.com>
2024-04-04 18:24:10 +0200

    chore: changed instance type (t2.small -> t2.micro)

--- apps/hello-world-microservice/src/app.service.ts
+++ apps/hello-world-microservice/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello from first microservice (yes its really, a microservice on EC2 instance)!';
+    return 'Hello from first microservice (yes its really, a microservice on EC2 instance) !';
   }
 }
